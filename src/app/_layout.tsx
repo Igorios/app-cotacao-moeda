edc95da5dc1c51bbd04dc5fe16dde162a600768c
Igor Borges <borgesigor153@gmail.com>
2024-07-28 14:14:42 -0300

    :tada: primeiro commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Slot } from "expo-router";
+import "@/src/styles/global.css"
+
+export default function RootLayout() {
+  return (
+    <Slot />
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+    className="mt-10 p-10"
+    >
+      <Text className="text-red-600">Olá mundo! Lorem ipsum dolor sit.</Text>
+      <Text className="text-green-500">Lorem ipsum, dolor sit amet.</Text>
+    </View>
+  );
+}
