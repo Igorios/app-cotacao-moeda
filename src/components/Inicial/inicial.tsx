777d6facacb2978b5c24a5d9729fe27ac20e247b
Igor Borges <borgesigor153@gmail.com>
2024-08-06 21:29:54 -0300

    :recycle: refactor: implementando o loading da pagina inicial

--- src/components/Inicial/inicial.tsx
+++ src/components/Inicial/inicial.tsx
@@ -1,38 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { formataMoeda } from "@/src/utils/FormataDinheiro";
 import { useMoedas } from "@/src/app/data/hooks/moedas";
 import DetalheMoeda from "../DetalheMoeda";
+import Loading from "../Loading";
 
 export default function Inicial() {
+
   const { moeda, getMoedas } = useMoedas();
+
+  const [loading, setLoading] = useState<boolean>(true);
 
   const dados = moeda ? moeda[Object.keys(moeda)[0]] : 0;
 
   useEffect(() => {
-    getMoedas("USD");
-  }, [getMoedas]);
+
+    const fetchData = async () => { 
+      await getMoedas("USD");
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <>
-      <View className="bg-bgSecondy mt-8 mx-2 rounded-md">
-        <View>
-          <Text className="text-start text-white pl-5 pt-4 text-xl">
-            Valor do dólar hoje:
-          </Text>
+    {loading ? (
+      <Loading />
+    ): (
+      <>
+
+        <View className="bg-bgSecondy mt-8 mx-2 rounded-md">
+          <View>
+            <Text className="text-start text-white pl-5 pt-4 text-xl">
+              Valor do dólar hoje:
+            </Text>
+          </View>
+          <View className="w-11/12 border-b border-white pt-4 mx-auto text-center" />
+          <View className="my-28">
+            <Text className="p-5 text-white text-center text-5xl font-semibold">
+              {formataMoeda(dados?.bid)}
+            </Text>
+
+            {/* <View className="w-2/4 border-b border-white mx-auto text-center" /> */}
+
+          </View>
         </View>
-        <View className="w-11/12 border-b border-white pt-4 mx-auto text-center" />
-        <View className="my-28">
-          <Text className="p-5 text-white text-center text-5xl font-semibold">
-            {formataMoeda(dados?.bid)}
-          </Text>
 
-          {/* <View className="w-2/4 border-b border-white mx-auto text-center" /> */}
-
-        </View>
-      </View>
-
-      <DetalheMoeda dadosMoeda={dados} />
+        <DetalheMoeda dadosMoeda={dados} />
+      
+      </>
+    )}
 
     </>
   );
