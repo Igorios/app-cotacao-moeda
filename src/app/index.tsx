db21186f5b93a5207e21bf92d8b63aa4b2e6eac7
Igor Borges <borgesigor153@gmail.com>
2024-08-10 16:11:45 -0300

    :sparkles: feat: botao para gerar notificacao no app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,53 @@
-import { ScrollView, Text, View } from "react-native";
+import { Alert, Button, ScrollView, Text, View } from "react-native";
 import Header from "../components/Header";
 import Constants from "expo-constants";
 import Inicial from "../components/Inicial/inicial";
-import DetalheMoeda from "../components/DetalheMoeda";
+import * as Notifications from 'expo-notifications';
 
 const statusBarHeight = Constants.statusBarHeight;
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+
 export default function Index() {
+
+  const handleCallNotification = async () => {
+    const { status } = await Notifications.getPermissionsAsync();
+    
+
+    if (status !== 'granted') {
+      const { status: newStatus } = await Notifications.requestPermissionsAsync();
+      if (newStatus !== 'granted') {
+        Alert.alert(
+          'Permissão necessária',
+          'As notificações precisam ser permitidas para que você receba alertas importantes.',
+        );
+      } else {
+        Alert.alert(
+          'Permissão concedida',
+          'Você receberá notificações do aplicativo.',
+        );
+      }
+    }
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Olá galera do app!",
+        body: "Notificação do seu app de cotação!"
+      },
+      trigger: {
+        seconds: 3
+      }
+    });
+
+  }
+
   return (
     <ScrollView className="bg-bgMain" style={{ flex: 1 }}>
 
@@ -15,6 +56,14 @@ export default function Index() {
       </View>
 
       <Inicial />
+
+      <View className="mt-5">
+        <Button 
+        onPress={handleCallNotification}
+          title="Gerar Notificação" 
+        />
+      </View>
+
       
     </ScrollView>
   );
